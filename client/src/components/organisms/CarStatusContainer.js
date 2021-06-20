2eca7a69f4d59ca61039d5a3ba7e28e7f7580a8b
unknown <sergio-caballero_student2021@wilder.school>
2021-06-20 14:29:32 +0200

    added conditional

--- client/src/components/organisms/CarStatusContainer.js
+++ client/src/components/organisms/CarStatusContainer.js
@@ -2,7 +2,6 @@ import React, {useContext} from 'react'
 import DivColumn from '../atoms/DivColumn'
 import H2 from '../atoms/H2'
 import { CarStatus } from '../molecules/CarStatus'
-import {textData} from '../../data/textData'
 import { ApiContext } from '../../context/ApiContext';
 
 
@@ -12,20 +11,18 @@ export const CarStatusContainer = () => {
     /* console.log(context.user[0].cars[0].variableFeatures.generalStatus) */
    
     return (
+
+        
         <DivColumn>
+            
             <H2>Estado general</H2>
 
-
+           
             {context.user[0].cars[0].variableFeatures.generalStatus.map((status, index)=>
                 <CarStatus
                 key = {index}
                 name = {status.name} 
-                status = {status.status}/>)}
-            {/* {textData.CarInfo[0].carStatus.map((car, index)=>
-                <CarStatus
-                key = {index}
-                name = {car.name} 
-                status = {car.status}/>)} */}
+                status = {status.status}/>)})
             
         </DivColumn>
     )
--- client/src/components/pages/Car.js
+++ client/src/components/pages/Car.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import MainContainer from '../atoms/MainContainer'
 import PageContainer from '../atoms/PageContainer'
 import { CarStatusContainer } from '../organisms/CarStatusContainer'
+import { ApiContext } from '../../context/ApiContext';
 
 
 export const Car = () => {
+    const context = useContext(ApiContext);
     return (
         <PageContainer>
             <MainContainer>
-                <CarStatusContainer/>
+                {context.usersLoaded&&
+                <CarStatusContainer/>}
 
             </MainContainer>
         
