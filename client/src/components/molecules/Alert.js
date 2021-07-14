8f3817ce23d8209def74a86b485f7c7df10fe498
unknown <sergio-caballero_student2021@wilder.school>
2021-07-14 21:30:41 +0200

    stylesalert

--- client/src/components/molecules/Alert.js
+++ client/src/components/molecules/Alert.js
@@ -50,15 +50,12 @@ const Alert = (props) => {
                 <Div column span><Span success={props.status==="success"? true: false} warning={props.status==="warning"? true: false} danger={props.status==="danger"? true: false}>{icons[selectIcon(props.title, props.type)] }</Span></Div>
                 <Div column center padding>
                     
-                    
                   {/*  <H3 secondary>{props.dueDate && dayjs(props.dueDate).format('DD/MM/YYYY')}</H3> */} 
                     <H3>{props.type} / {props.title}</H3> 
-                   
-
+                
                     {(isDisplaying && props.dueDate) && <P alertDescription> Fecha límite: {dayjs(props.dueDate).format('DD/MM/YYYY')}</P>}
                     {isDisplaying && (<P alertDescription>{props.description}</P>)}
                     {isDisplaying && (<Img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL9lsmUsiO6xeqX88YLXk3_FsOiLMmV3WnYA&usqp=CAU"></Img>)}
-                    
                     
                 </Div>
                     
