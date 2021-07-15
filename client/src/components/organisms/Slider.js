80ed52d05445bcc4f3ec7509e634330b544cce01
Alopmel <78558722+Alopmel@users.noreply.github.com>
2021-07-15 22:35:32 +0200

    doit

--- client/src/components/organisms/Slider.js
+++ client/src/components/organisms/Slider.js
@@ -13,9 +13,23 @@ import sortBySeverity from '../../utils/sortBySeverity';
 import SuscriptionType from '../molecules/SuscriptionType';
 
 
+// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-// Import Swiper styles
-import 'swiper/swiper.scss';
+
+import "swiper/swiper.min.css";
+import "swiper/components/navigation/navigation.min.css"
+
+
+
+// import Swiper core and required modules
+import SwiperCore, {
+  Navigation
+} from 'swiper/core';
+
+// install Swiper modules
+SwiperCore.use([Navigation]);
+
+
 
 
 function Slider (props) {
@@ -23,67 +37,64 @@ function Slider (props) {
    const context = useContext(ApiContext);  
 
    return (
-       <div>
-       </div>
-    // <Swiper
-    //   spaceBetween={50}
-    //   slidesPerView={1}
-    //   onSlideChange={() => console.log('slide change')}
-    //   onSwiper={(swiper) => console.log(swiper)}
-    // >
-      
-    //     {context.carStatusLoaded && context.user[0].cars.map(cars => {console.log(cars)
-    //     console.log(cars);
-    //         return(
-    //     <SwiperSlide key={nanoid()}>
+       
+    <Swiper
+    spaceBetween={0}
+    slidesPerView={1}
+    onSlideChange={() => console.log('slide change')}
+    onSwiper={(swiper) => console.log(swiper)}
+  >
+       {context.carStatusLoaded && context.user[0].cars.map(cars => (
+                
+       <SwiperSlide key={cars.id} className="slide">
 
+          
+           <Div column center>
+               <Div img>
+                   <Img src={cars.staticFeatures.Image}/>
+               </Div>
+                   <H2>{cars.staticFeatures.brand} {cars.staticFeatures.model}</H2>
+                   <P>{cars.staticFeatures.year} - {cars.variableFeatures.kM} kms</P>
+           </Div>
             
-    //         <Div column center>
-    //             <Div img>
-    //                 <Img src={cars.staticFeatures.Image}/>
-    //             </Div>
-    //                 <H2>{cars.staticFeatures.brand} {cars.staticFeatures.model}</H2>
-    //                 <P>{cars.staticFeatures.year} - {cars.variableFeatures.kM} kms</P>
-    //         </Div>
+           <Div column>           
+               <Div row spaceBet>
+                   <H3>Kilómetros/año</H3>
+                   <H3>{cars.variableFeatures.kM} kms</H3>
+               </Div >
+               <Div row spaceBet>
+               <H3>Valor de vehículo</H3>
+               <H3>{cars.variableFeatures.currentValue} €</H3>
+               </Div>
+           </Div >
             
-    //         <Div column>           
-    //             <Div row spaceBet>
-    //                 <H3>Kilómetros/año</H3>
-    //                 <H3>{cars.variableFeatures.kM} kms</H3>
-    //             </Div >
-    //             <Div row spaceBet>
-    //             <H3>Valor de vehículo</H3>
-    //             <H3>{cars.variableFeatures.currentValue} €</H3>
-    //             </Div>
-    //         </Div >
+           <Div column>
+           <SuscriptionType {...props}/>
+           </Div>
             
-    //         <Div column>
-    //         <SuscriptionType {...props}/>
-    //         </Div>
-            
-    //         <Div column>
-    //         <H2>Avisos pendientes</H2>
+           <Div column>
+           <H2>Avisos pendientes</H2>
 
-    //         {context.carStatusLoaded && context.carStatus.flat().sort((a,b)=>sortBySeverity(a.status)-(sortBySeverity(b.status))).map(elem=>{
-    //            console.log(elem);
-    //            return ( 
-    //                 elem.status!=="success" && 
-    //                 <Alert
-    //                     key={nanoid()}
-    //                     dueDate = {elem.dueDate}
-    //                     title= {elem.title}
-    //                     description={elem.statusDescription}
-    //                     type={elem.type} 
-    //                     status={elem.status}  
-    //                 />
-    //             )
+           {context.carStatusLoaded && context.carStatus.flat().sort((a,b)=>sortBySeverity(a.status)-(sortBySeverity(b.status))).map(elem=>{
+              console.log(elem);
+              return ( 
+                   elem.status!=="success" && 
+                   <Alert
+                       key={nanoid()}
+                       dueDate = {elem.dueDate}
+                       title= {elem.title}
+                       description={elem.statusDescription}
+                       type={elem.type} 
+                       status={elem.status}  
+                   />
+               )
 
-    //             })}                
-    //     </Div>
-    //     </SwiperSlide>
+               })}                
+       </Div>
+       </SwiperSlide>
         
-    //      ) })}
-    // </Swiper>
+       ))}
+   </Swiper>
 
   );
 };
