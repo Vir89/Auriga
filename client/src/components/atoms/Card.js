f4c4be169e87bdbaf85402bf4157be6be3e0f1c5
SergKcode <76130551+SergKcode@users.noreply.github.com>
2021-07-20 17:47:58 +0200

    Update Card.js

--- client/src/components/atoms/Card.js
+++ client/src/components/atoms/Card.js
@@ -2,8 +2,8 @@ import styled, {css} from "styled-components";
 
 const Card = styled.div `
 	background-color: #fff;
-	border-radius: 10px;
-	box-shadow: 0 10px 10px rgba(0, 0, 0, 0.2);
+	border-radius: 50px;
+	box-shadow: 0 10px 10px rgba(0, 0, 0, 0.2); 
 	display: flex;
 	max-width: 95%;
 	margin: 10px;
